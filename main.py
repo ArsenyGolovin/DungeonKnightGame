49edb87448f6555fd6efb6daa29af1920b0fa928
Арсений <argo24.11.05@gmail.com>
2021-12-28 11:48:55 +0300

    Главный цикл перенесён в класс Board

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import pygame
-import sys
 import os
+import sys
+
+import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((900, 900), pygame.NOFRAME)
@@ -28,154 +29,208 @@ def terminate():
     sys.exit()
 
 
-player_sword_image = load_image('sword.png', -1)
-player_kope_image = load_image('kope.png', -1)
-player_axe_image = load_image('axe.png', -1)
-wall_image = load_image('box.jpg')
-floor_image = load_image('floor0.jpg')
-door_image = load_image('door.png')
-door2_image = load_image('door2.png')
-shop_image = load_image('shop.png', -1)
+class Player:
+    CHAR = 'P'
 
 
-def load_level(filename):
-    filename = "data/" + filename
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
-    max_width = max(map(len, level_map))
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+class PlayerSword(Player):
+    IMAGE = load_image('sword.png', -1)
+    CHAR = 'S'
+
+    def __init__(self):
+        self.hp = 100
+        self.dmg = 20
+        self.armor = 10
+        self.attack_speed_per_second = 4
 
 
-def draw_img(obj, x, y):
-    img: pygame.Surface
-    if obj == 'player_sword':
-        img = pygame.transform.scale(player_sword_image, (70, 70))
-    elif obj == 'player_kope':
-        img = pygame.transform.scale(player_kope_image, (70, 70))
-    elif obj == 'player_axe':
-        img = pygame.transform.scale(player_axe_image, (70, 70))
-    elif obj == 'wall':
-        img = pygame.transform.scale(wall_image, (70, 70))
-    elif obj == 'floor':
-        img = pygame.transform.scale(floor_image, (70, 70))
-    elif obj == 'door':
-        img = pygame.transform.scale(door_image, (70, 70))
-    elif obj == 'door2':
-        img = pygame.transform.scale(door2_image, (70, 70))
-    elif obj == 'shop':
-        img = pygame.transform.scale(shop_image, (70, 70))
-    screen.blit(img, (x * 70, y * 70))
+class PlayerAxe(Player):
+    IMAGE = load_image('axe.png', -1)
+    CHAR = 'A'
+
+    def __init__(self):
+        self.hp = 120
+        self.dmg = 40
+        self.armor = 25
+        self.attack_speed_per_second = 2
 
 
-def draw_level(level):
-    for y in range(len(level)):
-        for x in range(len(level[y])):
-            if level[y][x] == '0':
-                draw_img('floor', x, y)
-            elif level[y][x] == '1':
-                draw_img('wall', x, y)
-            elif level[y][x] == 'S':
-                draw_img('floor', x, y)
-                draw_img('player_sword', x, y)
-            elif level[y][x] == 'K':
-                draw_img('floor', x, y)
-                draw_img('player_kope', x, y)
-            elif level[y][x] == 'A':
-                draw_img('floor', x, y)
-                draw_img('player_axe', x, y)
-            elif level[y][x] == '2':
-                draw_img('door', x, y)
-            elif level[y][x] == '3':
-                draw_img('door2', x, y)
-            elif level[y][x] == 'H':
-                draw_img('floor', x, y)
-                draw_img('shop', x, y)
+class PlayerKope(Player):
+    IMAGE = load_image('kope.png', -1)
+    CHAR = 'K'
+
+    def __init__(self):
+        self.hp = 90
+        self.dmg = 50
+        self.armor = 10
+        self.attack_speed_per_second = 1.5
 
 
-def show_start_screen():
-    intro_text = ["           Dungeon Knight",
-                  "",
-                  "",
-                  "",
-                  "",
-                  ""]
+class Board:
+    wall_image = load_image('box.jpg')
+    floor_image = load_image('floor0.jpg')
+    door_image = load_image('door.png')
+    door2_image = load_image('door2.png')
+    shop_image = load_image('shop.png', -1)
 
-    fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
-    screen.blit(fon, (0, 0))
-    font = pygame.font.Font('data/ebrima.ttf', 60)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, True, pygame.Color('orange'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
+    def __init__(self):
+        self.current_level = self.load_level('map.txt')
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                return
-        pygame.display.flip()
+    def show_start_screen(self):
+        intro_text = ["           Dungeon Knight",
+                      "",
+                      "",
+                      "",
+                      "",
+                      ""]
+        fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
+        screen.blit(fon, (0, 0))
+        font = pygame.font.Font('data/ebrima.ttf', 60)
+        text_coord = 50
+        for line in intro_text:
+            string_rendered = font.render(line, True, pygame.Color('orange'))
+            intro_rect = string_rendered.get_rect()
+            text_coord += 10
+            intro_rect.top = text_coord
+            intro_rect.x = 10
+            text_coord += intro_rect.height
+            screen.blit(string_rendered, intro_rect)
+
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+                elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                    running = False
+            pygame.display.flip()
+        self.mainloop()
+
+    @staticmethod
+    def load_level(filename):
+        return Level(filename)
+
+    def draw_level(self):
+        field = self.current_level.get_field()
+        for y in range(len(field)):
+            for x in range(len(field[y])):
+                if field[y][x] == '0':
+                    Board.draw_img('floor', x, y)
+                elif field[y][x] == '1':
+                    Board.draw_img('wall', x, y)
+                elif field[y][x] == PlayerSword.CHAR:
+                    Board.draw_img('floor', x, y)
+                    Board.draw_img('player_sword', x, y)
+                elif field[y][x] == 'K':
+                    Board.draw_img('floor', x, y)
+                    Board.draw_img('player_kope', x, y)
+                elif field[y][x] == 'A':
+                    Board.draw_img('floor', x, y)
+                    Board.draw_img('player_axe', x, y)
+                elif field[y][x] == '2':
+                    Board.draw_img('door', x, y)
+                elif field[y][x] == '3':
+                    Board.draw_img('door2', x, y)
+                elif field[y][x] == 'H':
+                    Board.draw_img('floor', x, y)
+                    Board.draw_img('shop', x, y)
+
+    @staticmethod
+    def draw_img(obj, x, y):
+        img: pygame.Surface
+        size = (70, 70)
+        if obj == 'player_sword':
+            img = pygame.transform.scale(PlayerSword.IMAGE, size)
+        elif obj == 'player_kope':
+            img = pygame.transform.scale(PlayerKope.IMAGE, size)
+        elif obj == 'player_axe':
+            img = pygame.transform.scale(PlayerAxe.IMAGE, size)
+        elif obj == 'wall':
+            img = pygame.transform.scale(Board.wall_image, size)
+        elif obj == 'floor':
+            img = pygame.transform.scale(Board.floor_image, size)
+        elif obj == 'door':
+            img = pygame.transform.scale(Board.door_image, size)
+        elif obj == 'door2':
+            img = pygame.transform.scale(Board.door2_image, size)
+        elif obj == 'shop':
+            img = pygame.transform.scale(Board.shop_image, size)
+        screen.blit(img, (x * size[0], y * size[1]))
+
+    def mainloop(self):
+        running = True
+        FPS = 60
+        current_level = self.current_level
+        current_player = PlayerSword()
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        current_level.move_player('left', current_player)
+                    if event.key == pygame.K_RIGHT:
+                        current_level.move_player('right', current_player)
+                    if event.key == pygame.K_UP:
+                        current_level.move_player('up', current_player)
+                    if event.key == pygame.K_DOWN:
+                        current_level.move_player('down', current_player)
+
+            screen2.fill('black')
+            self.draw_level()
+            screen.blit(screen2, (0, 0))
+            pygame.display.flip()
 
 
+class Level:
+    def __init__(self, name: str):
+        with open('data/' + name) as mapFile:
+            level_map = [line.strip() for line in mapFile]
+        max_width = max(map(len, level_map))
+        self.field = list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+    def get_field(self):
+        return self.field
+
+    def get_coords(self, elem: str):
+        for y, s in enumerate(self.field):
+            for x, e in enumerate(s):
+                if self.field[y][x] == elem:
+                    return x, y
+
+    def move_player(self, direction: str, player: Player):
+        char = player.CHAR
+        x, y = self.get_coords(char)
+        if direction == 'up':
+            if self.field[y - 1][x] == '0':
+                self.field[y] = list(self.field[y])
+                self.field[y - 1] = list(self.field[y - 1])
+                self.field[y - 1][x] = char
+                self.field[y][x] = '0'
+                self.field[y] = ''.join(self.field[y])
+                self.field[y - 1] = ''.join(self.field[y - 1])
+        elif direction == 'down':
+            if self.field[y + 1][x] == '0':
+                self.field[y] = list(self.field[y])
+                self.field[y + 1] = list(self.field[y + 1])
+                self.field[y + 1][x] = char
+                self.field[y][x] = '0'
+                self.field[y] = ''.join(self.field[y])
+                self.field[y + 1] = ''.join(self.field[y + 1])
+        elif direction == 'right':
+            if self.field[y][x + 1] == '0':
+                s = list(self.field[y])
+                s[x + 1], s[x] = char, '0'
+                self.field[y] = ''.join(s)
+        elif direction == 'left':
+            if self.field[y][x - 1] == '0':
+                s = list(self.field[y])
+                s[x - 1], s[x] = char, '0'
+                self.field[y] = ''.join(s)
+
+
+board = Board()
 width, height = 840, 770
 screen2 = pygame.display.set_mode((width, height), pygame.NOFRAME)
-screen2.fill((255, 255, 255))
-running = True
-FPS = 50
-show_start_screen()
-level = load_level('map.txt')
-
-
-def get_coords(level, elem: str):
-    for y, s in enumerate(level):
-        for x, e in enumerate(s):
-            if level[y][x] == elem:
-                return x, y
-
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x, y = get_coords(level, 'S')
-                if level[y][x - 1] == '0':
-                    s = list(level[y])
-                    s[x - 1], s[x] = 'S', '0'
-                    level[y] = ''.join(s)
-            if event.key == pygame.K_RIGHT:
-                x, y = get_coords(level, 'S')
-                if level[y][x + 1] == '0':
-                    s = list(level[y])
-                    s[x + 1] = 'S'
-                    s[x] = '0'
-                    level[y] = ''.join(s)
-            if event.key == pygame.K_UP:
-                x, y = get_coords(level, 'S')
-                if level[y - 1][x] == '0':
-                    level[y] = list(level[y])
-                    level[y - 1] = list(level[y - 1])
-                    level[y - 1][x] = 'S'
-                    level[y][x] = '0'
-                    level[y] = ''.join(level[y])
-                    level[y - 1] = ''.join(level[y - 1])
-            if event.key == pygame.K_DOWN:
-                x, y = get_coords(level, 'S')
-                if level[y + 1][x] == '0':
-                    level[y] = list(level[y])
-                    level[y + 1] = list(level[y + 1])
-                    level[y + 1][x] = 'S'
-                    level[y][x] = '0'
-                    level[y] = ''.join(level[y])
-                    level[y + 1] = ''.join(level[y + 1])
-
-    screen2.fill('black')
-    draw_level(level)
-    screen.blit(screen2, (0, 0))
-    pygame.display.flip()
+screen2.fill('black')
+board.show_start_screen()
